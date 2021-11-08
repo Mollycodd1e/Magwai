ae10a5a55e5d10da7a3bb3568662b2434a03c86d
Mollycodd1e <selik7@mail.ru>
2021-11-08 17:25:20 +0300

    Добавляет десктопную шапку проекта

---
+++ source/js/toggle.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+  var navMain = document.querySelector('.main-nav');
+
+  if (navMain) {
+    var navToggle = document.querySelector('.main-nav__toggle');
+
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.add('main-nav--opened');
+        navMain.classList.remove('main-nav--closed');
+      } else {
+        navMain.classList.remove('main-nav--opened');
+        navMain.classList.add('main-nav--closed');
+      }
+    });
+  }
+})();
